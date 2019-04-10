143a2af915c2e179b4862603400fb14184b9cd7a
oh changehee <eduochange@gmail.com>
2019-04-10 04:26:22 +0000

    로그인,로그아웃 + base.html링크수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
 # Create your views here.
 def signup(request):
     if request.method=="POST":
@@ -9,5 +12,31 @@ def signup(request):
             return redirect("posts:list")
     else:
         form = UserCreationForm()
-    return render(request, 'accounts/signup.html',{'form':form})
+    return render(request, 'accounts/form.html',{'form':form})
+    
+def login(request):
+    if request.method=="POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("posts:list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/form.html", {"form":form})
+    
+def logout(request):
+    auth_logout(request)
+    return redirect("posts:list")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
