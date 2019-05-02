4dee97e73fa62f5d200d0a5a1de0c03b928f347a
eduChang <educhange@gmail.com>
2019-05-02 11:01:16 +0900

    test

--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,6 @@ from .models import Post, Comment, Hashtag
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
-# Create your views here.
 def list(request):
     posts = Post.objects.all()
     comment_form = CommentForm()
